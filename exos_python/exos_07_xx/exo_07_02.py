58235e2f61507bef0770141e85ca2d61983b5036
Mounir <mounirlaity@gmail.com>
2022-09-20 16:03:17 +0200

    finished 7.2 and 7.3

---
+++ exos_python/exos_07_xx/exo_07_02.py
@@ -0,0 +1,5 @@
+def ligne_car(n, ca):
+    return n * ca
+
+
+print(ligne_car(10, "pl"))
---
+++ exos_python/exos_07_xx/exo_07_03.py
@@ -0,0 +1,8 @@
+from math import pi
+
+
+def surf_cercle(r):
+    return 2 * pi * r**2
+
+
+print(surf_cercle(30))
