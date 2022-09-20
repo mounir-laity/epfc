e06f91c61bd3ca79e7048d7a5c611ba818c8d1d6
Mounir <mounirlaity@gmail.com>
2022-09-20 16:06:26 +0200

    finished 7.5

--- exos_python/exos_07_xx/exo_07_04.py
+++ exos_python/exos_07_xx/exo_07_04.py
@@ -2,4 +2,4 @@ def vol_boite(x1, x2, x3):
     return x1 * x2 * x3
 
 
-print(vol_boite(4, 5, 6))
+print(vol_boite(5.2, 7.7, 3.3))
---
+++ exos_python/exos_07_xx/exo_07_05.py
@@ -0,0 +1,5 @@
+def maximum(n1, n2, n3):
+    return max(n1, n2, n3)
+
+
+print(maximum(4, -10, 2))
