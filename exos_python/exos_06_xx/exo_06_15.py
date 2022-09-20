e27cc388727194df0bc0a660a0ce70e9005bb8f2
Mounir <mounirlaity@gmail.com>
2022-09-20 15:59:09 +0200

    finished 6.15

---
+++ exos_python/exos_06_xx/exo_06_15.py
@@ -0,0 +1,16 @@
+from statistics import mean
+from math import inf
+
+notes = []
+count = 0
+while True:
+    note = float(input("Please enter a note :\n"))
+    if note < 0:
+        print(notes)
+        break
+    notes.append(note)
+    count += 1
+    print(f"Nombre de notes : {count}")
+    print(f"Maximum des notes : {max(notes)}")
+    print(f"Minimum des notes : {min(notes)}")
+    print(f"Moyenne des notes : {mean(notes)}")
