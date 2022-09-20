0ea1ed010554af0f95600f162732e3d898332e09
Mounir <mounirlaity@gmail.com>
2022-09-20 15:47:55 +0200

    finished exo 6.14

---
+++ exos_python/exos_06_xx/exo_06_14.py
@@ -0,0 +1,12 @@
+names = [
+    "Jean-Michel",
+    "Marc",
+    "Vanessa",
+    "Anne",
+    "Maximilien",
+    "Alexandre-Benoît",
+    "Louise",
+]
+
+for name in names:
+    print(f"The name '{name}' has a length of {len(name)} characters.")
