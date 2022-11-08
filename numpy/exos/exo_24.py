05eaf13d4c9e21f5a2326465d9773f402cb2c78c
Mounir <mounirlaity@gmail.com>
2022-11-08 16:53:09 +0100

    added exo 24

---
+++ numpy/exos/exo_24.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+vector1 = np.random.randint(low=0,high=10, size=5)
+vector2 = np.random.randint(low=0,high=10, size=5)
+print(vector1)
+print(vector2)
+print(np.multiply(vector1, vector2))
