d865b5dc5ae72bb1c91b1e788af6ed98c57ab82d
Mounir <mounirlaity@gmail.com>
2022-11-15 16:42:13 +0100

    exo 47

---
+++ numpy/exos/exo_47.py
@@ -0,0 +1,6 @@
+"""Écrivez un programme NumPy pour créer un tableau unidimensionnel de quarante valeurs générées
+pseudo-aléatoirement. Sélectionnez des nombres aléatoires à partir d'une distribution uniforme entre 0
+et 1."""
+import numpy as np
+
+print(np.random.rand(40))
