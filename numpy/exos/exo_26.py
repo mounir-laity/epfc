f6797ff35dc228e698df4b3d61f37e3448f64ee1
Mounir <mounirlaity@gmail.com>
2022-11-15 14:01:19 +0100

    finished exo 26

---
+++ numpy/exos/exo_26.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def find_dimensions(array):
+    shape = array.shape
+    if shape:
+        if len(shape) < 2:
+            return (1,shape[0])
+        else:
+            return (shape[0], shape[-1])
+    print(shape)
+    
+    
+array = np.array([[4,5,6],[7,8,9],[10,11,12],[1,2,3]])
+lines, columns = find_dimensions(array)
+print(f"There are {lines} lines and {columns} columns in that array.")
