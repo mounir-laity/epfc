2e68a3844f6bb5095b7280dd9ec2734e74c46d6f
Mounir <mounirlaity@gmail.com>
2022-11-15 14:25:18 +0100

    finished exo 29

---
+++ numpy/exos/exo_29.py
@@ -0,0 +1,7 @@
+"""Écrivez un programme NumPy pour créer une matrice zéro 5x5 avec des éléments sur la diagonale
+principale égaux à 1, 2, 3, 4, 5"""
+import numpy as np
+
+arr = np.zeros((5,5))
+np.fill_diagonal(arr,[1,2,3,4,5])
+print(arr)
