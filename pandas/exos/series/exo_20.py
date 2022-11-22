98eb30db990d0992f962f19f706a180150d08954
Mounir <mounirlaity@gmail.com>
2022-11-22 16:50:24 +0100

    added exo 20

--- pandas/exos/series/exo_20.py
+++ pandas/exos/series/exo_20.py
@@ -1,8 +1,9 @@
 import pandas as pd
-
-ser = pd.Series([1,4,5,6,3,5,4,5,8,9,6,2,1,3,6,7])
-
-occurences = ser.value_counts()
-for element in occurences:
-    if occurences[element] == occurences[0]:
-        pass
+import numpy as np
+np.random.RandomState(100)
+num_series = pd.Series(np.random.randint(1, 5, [15]))
+print("Original Series:")
+print(num_series)
+print("Top 2 Freq:", num_series.value_counts())
+result = num_series[~num_series.isin(num_series.value_counts().index[:1])] ='Other'
+print(num_series)
