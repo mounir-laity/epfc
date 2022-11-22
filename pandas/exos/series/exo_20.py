61031d0d0fe475e73a9896c76848e437b3ab0f1f
Mounir <mounirlaity@gmail.com>
2022-11-22 16:44:31 +0100

    added plot to exo 19

--- pandas/exos/series/exo_20.py
+++ pandas/exos/series/exo_20.py
@@ -1,9 +1,8 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 
 ser = pd.Series([1,4,5,6,3,5,4,5,8,9,6,2,1,3,6,7])
 
 occurences = ser.value_counts()
-print(occurences)
-plt.hist(ser)
-plt.show()
+for element in occurences:
+    if occurences[element] == occurences[0]:
+        pass
