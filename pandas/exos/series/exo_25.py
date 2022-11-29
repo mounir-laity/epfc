498a998fd8249abb3c8e2f5711735c3b29ddc1bc
Mounir <mounirlaity@gmail.com>
2022-11-29 14:02:33 +0100

    added exo 25

--- pandas/exos/series/exo_25.py
+++ pandas/exos/series/exo_25.py
@@ -2,5 +2,5 @@ import numpy as np
 import pandas as pd
 
 ser = pd.Series(data=["php","python","c#","15","mounir","paSSwOrd"])
-ser = ser.map(lambda x: x[0].upper()+x[1:-1]+x[-1].upper())
+ser = ser.map(lambda x: len(x))
 print(ser)
