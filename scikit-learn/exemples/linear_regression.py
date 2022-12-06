bf95de12274628dd892e830ebf0133b6b21aea18
Mounir <mounirlaity@gmail.com>
2022-12-06 16:40:05 +0100

    added color map

--- scikit-learn/exemples/linear_regression.py
+++ scikit-learn/exemples/linear_regression.py
@@ -5,10 +5,9 @@ from sklearn.linear_model import SGDRegressor
 np.random.seed(0)
 x, y = make_regression(n_samples=100, n_features=1,
 noise=10)
-plt.scatter(x, y, c="red")
+plt.scatter(x, y,c=x,cmap=plt.plasma())
 model = SGDRegressor(max_iter=1000, eta0=0.01) # Play with these parameters to get the best result possible
 model.fit(x,y)
 print('Coeff R2 =', model.score(x, y))
-plt.scatter(x, y)
-plt.plot(x, model.predict(x), c='orange', lw = 3)
+plt.plot(x, model.predict(x), color='green', lw = 3)
 plt.show()
