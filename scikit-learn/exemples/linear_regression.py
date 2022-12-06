caf107917d7adb6c2d524ae153b0f2cf5df547fb
Mounir <mounirlaity@gmail.com>
2022-12-06 16:27:05 +0100

    added small comment

--- scikit-learn/exemples/linear_regression.py
+++ scikit-learn/exemples/linear_regression.py
@@ -6,7 +6,7 @@ np.random.seed(0)
 x, y = make_regression(n_samples=100, n_features=1,
 noise=10)
 plt.scatter(x, y, c="red")
-model = SGDRegressor(max_iter=1000, eta0=0.01)
+model = SGDRegressor(max_iter=1000, eta0=0.01) # Play with these parameters to get the best result possible
 model.fit(x,y)
 print('Coeff R2 =', model.score(x, y))
 plt.scatter(x, y)
