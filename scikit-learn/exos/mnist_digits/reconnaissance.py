998e85338e2dd4cf2027519220f107ebb2f33149
Mounir <mounirlaity@gmail.com>
2022-12-20 16:03:10 +0100

    added example with the mnist dataset

---
+++ scikit-learn/exos/mnist_digits/reconnaissance.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+
+def predict_image(index, df_test):
+    image = df_test.iloc[index]
+    test = image.values.reshape(1, -1)
+    image = image.values.reshape(28,28)
+    plt.imshow(image, cmap = 'Greys_r')
+    print(f"L'image d'indice {index} est un {model.predict(test)[0]}")
+    plt.show(block=False)
+
+
+if __name__ == "__main__":
+    filename = "chiffres.csv"
+    df = pd.read_csv(filename)
+    df_train, df_test = train_test_split(df, train_size=0.8)
+    print(df_train.shape)
+    print(df_test.shape)
+    y_train = df_train["label"]
+    y_test = df_test["label"]
+    X_train = df_train.loc[:, df.columns != "label"]
+    X_test = df_test.loc[:, df.columns != "label"]
+
+    model = KNeighborsClassifier(n_neighbors=3)
+    model.fit(X_train,y_train)
+    score = model.score(X_test,y_test)
+    print(f"The score of the model is {score}")
+    index = 777
+    predict_image(index, X_test)
+    print(f"La bonne catégorie pour l'image d'indice {index} est {y_test.iloc[index]}")
+    input("Appuyez sur entrée pour quitter...")
